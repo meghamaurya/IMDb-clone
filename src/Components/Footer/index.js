148cb4dadacdd834d6bf1a2edb348e95899a0ac6
megha <meghamini40@gmail.com>
2022-08-30 15:39:53 +0530

    Footer component

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>About us
+                </p>
+            </footer>
+        </div>
+    )
+}
